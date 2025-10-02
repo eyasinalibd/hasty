3f340eadab8f5c5510379b285069cb63386ed3c3
eyasinalibd <eyasinali@gmail.com>
2025-10-02 16:58:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import io
 
 
 # --- Page Config ---
@@ -393,5 +394,6 @@ if st.session_state.logged_in:
 
         else:
             st.info("Please upload the HASTY Excel file to begin.")
+
 
 
